4d9d12492e208a619b045ba806c8df02dfad96d3
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-28 03:13:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game 
   
-  number = rand(1..6)
+  number = rand(6)+1
   puts "Enter a guess between 1 and 6!"
   guess = gets.chomp 
   case guess
@@ -8,6 +8,8 @@ def run_guessing_game
       puts "Goodbye!"
   when guess == number
    puts "You guessed the correct number!"
+  else 
+  puts "Sorry! The computer guessed #{number}."
  end
  
  
